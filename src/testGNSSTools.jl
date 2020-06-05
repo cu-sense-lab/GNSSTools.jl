82d680a94265e315cda09b927ef9905a07d5ddc6
Sergei Bilardi <sergei.bilardi@colorado.edu>
2020-06-05 17:39:15 -0600

    Set default value for dynamickf to true in demo method.

--- src/testGNSSTools.jl
+++ src/testGNSSTools.jl
@@ -13,7 +13,7 @@ function demo(;sigtype="l1ca", include_carrier=true, include_adc=true,
                include_noise=true, include_databits=true, simulatedata=true,
                saveto=missing, T="short", G=0.2, showplot=true, f_d=800.,
                fd_rate=0., prn=26, n0=1000., t_length=1e-3, M=4000,
-               fd_range=5000., dll_b=5., state_num=2, dynamickf=false,
+               fd_range=5000., dll_b=5., state_num=2, dynamickf=true,
                covMult=1., qₐ=100.)
     # Select signal type
     if sigtype == "l5q"
