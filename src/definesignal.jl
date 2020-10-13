da78bd49b73ebc1132f029a6550915a353cddea8
Sergei Bilardi <sergei.bilardi@colorado.edu>
2020-10-13 14:19:13 -0600

    Allowed user to change signal carrier frequency for all signal types. Defaults are the repective signal frequencies (e.g. L1 C/A signal defaults to L1_freq if sig_freq is not given to definesignal method. definesignal! defaults to the current initialized sig_freq.

--- src/definesignal.jl
+++ src/definesignal.jl
@@ -16,7 +16,7 @@ function definesignal(type::Val{:l1ca}, f_s, t_length; prn=1,
                       CN0=45., ϕ=0., nADC=4, B=2.046e6,
                       include_carrier=true, include_adc=true,
                       include_noise=true, code_start_idx=1,
-                      include_databits=true)
+                      include_databits=true, sig_freq=L1_freq)
     sample_num = Int(f_s * t_length)
     # Generate time vector
     t = calctvector(sample_num, f_s)
@@ -24,11 +24,11 @@ function definesignal(type::Val{:l1ca}, f_s, t_length; prn=1,
     db_code_length = Int64(ceil(l1ca_db_chipping_rate*t_length))
     # Calculate code chipping rates with Doppler applied
     # L1 C/A
-    f_l1ca_d = l1ca_chipping_rate*(1. + f_d/L1_freq)
-    f_l1ca_dd = l1ca_chipping_rate*fd_rate/L1_freq
+    f_l1ca_d = l1ca_chipping_rate*(1. + f_d/sig_freq)
+    f_l1ca_dd = l1ca_chipping_rate*fd_rate/sig_freq
     # Data bit sequence
-    f_db_d = l1ca_db_chipping_rate*(1. + f_d/L1_freq)
-    f_db_dd = l1ca_db_chipping_rate*fd_rate/L1_freq
+    f_db_d = l1ca_db_chipping_rate*(1. + f_d/sig_freq)
+    f_db_dd = l1ca_db_chipping_rate*fd_rate/sig_freq
     # Calculate the L1 C/A and data bit code phase offsets
     l1ca_init_code_phase = calcinitcodephase(l1ca_code_length,
                                              f_l1ca_d, f_l1ca_dd,
@@ -50,7 +50,7 @@ function definesignal(type::Val{:l1ca}, f_s, t_length; prn=1,
                       f_l1ca_d, f_l1ca_dd,
                       f_db_d, f_db_dd, sample_num,
                       isreplica, noexp, l1ca_chipping_rate,
-                      L1_freq, l1ca_code_length, db_code_length, databits)
+                      sig_freq, l1ca_code_length, db_code_length, databits)
 end
 
 
@@ -87,14 +87,15 @@ function definesignal!(signal::L1CASignal;
                        code_start_idx=signal.code_start_idx,
                        isreplica=signal.isreplica,
                        noexp=signal.noexp,
-                       include_databits=signal.include_databits)
+                       include_databits=signal.include_databits,
+                       sig_freq=signal.sig_freq)
     ## Calculate code chipping rates with Doppler applied
     # L1 C/A
-    f_l1ca_d = l1ca_chipping_rate*(1. + f_d/L1_freq)
-    f_l1ca_dd = l1ca_chipping_rate*fd_rate/L1_freq
+    f_l1ca_d = l1ca_chipping_rate*(1. + f_d/sig_freq)
+    f_l1ca_dd = l1ca_chipping_rate*fd_rate/sig_freq
     # Data bit sequence
-    f_db_d = l1ca_db_chipping_rate*(1. + f_d/L1_freq)
-    f_db_dd = l1ca_db_chipping_rate*fd_rate/L1_freq
+    f_db_d = l1ca_db_chipping_rate*(1. + f_d/sig_freq)
+    f_db_dd = l1ca_db_chipping_rate*fd_rate/sig_freq
     # Calculate the L1 C/A and data bit code phase offsets
     l1ca_init_code_phase = calcinitcodephase(l1ca_code_length,
                                             f_l1ca_d, f_l1ca_dd,
@@ -127,6 +128,7 @@ function definesignal!(signal::L1CASignal;
     signal.isreplica = isreplica
     signal.noexp = noexp
     signal.include_databits = include_databits
+    signal.sig_freq = sig_freq
     return signal
 end
 
@@ -148,17 +150,18 @@ function definesignal(type::Val{:l5q}, f_s, t_length; prn=1,
                       f_if=0., f_d=0., fd_rate=0., Tsys=535.,
                       CN0=45., ϕ=0., nADC=4, B=2.046e7,
                       include_carrier=true, include_adc=true,
-                      include_noise=true, code_start_idx=1)
+                      include_noise=true, code_start_idx=1,
+                      sig_freq=L5_freq)
     sample_num = Int(f_s * t_length)
     # Generate time vector
     t = calctvector(sample_num, f_s)
     # Calculate code chipping rates with Doppler applied
     # L5Q
-    f_l5q_d = L5_chipping_rate*(1. + f_d/L5_freq)
-    f_l5q_dd = L5_chipping_rate*fd_rate/L5_freq
+    f_l5q_d = L5_chipping_rate*(1. + f_d/sig_freq)
+    f_l5q_dd = L5_chipping_rate*fd_rate/sig_freq
     # Neuman sequence
-    f_nh_d = nh_chipping_rate*(1. + f_d/L5_freq)
-    f_nh_dd = nh_chipping_rate*fd_rate/L5_freq
+    f_nh_d = nh_chipping_rate*(1. + f_d/sig_freq)
+    f_nh_dd = nh_chipping_rate*fd_rate/sig_freq
     # Calculate the L5Q and nh code phase offsets
     l5q_init_code_phase = calcinitcodephase(L5_code_length,
                                             f_l5q_d, f_l5q_dd,
@@ -178,7 +181,7 @@ function definesignal(type::Val{:l5q}, f_s, t_length; prn=1,
                      f_l5q_d, f_l5q_dd,
                      f_nh_d, f_nh_dd, sample_num,
                      isreplica, noexp, L5_chipping_rate,
-                     L5_freq, L5_code_length)
+                     sig_freq, L5_code_length)
 end
 
 
@@ -213,14 +216,15 @@ function definesignal!(signal::L5QSignal;
                        include_noise=signal.include_noise,
                        code_start_idx=signal.code_start_idx,
                        isreplica=signal.isreplica,
-                       noexp=signal.noexp)
+                       noexp=signal.noexp,
+                       sig_freq=signal.sig_freq)
     ## Calculate code chipping rates with Doppler applied
     # L5Q
-    f_l5q_d = L5_chipping_rate*(1. + f_d/L5_freq)
-    f_l5q_dd = L5_chipping_rate*fd_rate/L5_freq
+    f_l5q_d = L5_chipping_rate*(1. + f_d/sig_freq)
+    f_l5q_dd = L5_chipping_rate*fd_rate/sig_freq
     # Neuman sequence
-    f_nh_d = nh_chipping_rate*(1. + f_d/L5_freq)
-    f_nh_dd = nh_chipping_rate*fd_rate/L5_freq
+    f_nh_d = nh_chipping_rate*(1. + f_d/sig_freq)
+    f_nh_dd = nh_chipping_rate*fd_rate/sig_freq
     # Calculate the L5Q and nh code phase offsets
     l5q_init_code_phase = calcinitcodephase(L5_code_length,
                                             f_l5q_d, f_l5q_dd,
@@ -252,6 +256,7 @@ function definesignal!(signal::L5QSignal;
     signal.code_start_idx = code_start_idx
     signal.isreplica = isreplica
     signal.noexp = noexp
+    signal.sig_freq = sig_freq
     return signal
 end
 
@@ -275,7 +280,7 @@ function definesignal(type::Val{:l5i}, f_s, t_length; prn=1,
                       CN0=45., ϕ=0., nADC=4, B=2.046e7,
                       include_carrier=true, include_adc=true,
                       include_noise=true, code_start_idx=1,
-                      include_databits=true)
+                      include_databits=true, sig_freq=L5_freq)
     sample_num = Int(f_s * t_length)
     # Generate time vector
     t = calctvector(sample_num, f_s)
@@ -283,15 +288,15 @@ function definesignal(type::Val{:l5i}, f_s, t_length; prn=1,
     db_code_length = Int64(ceil(L5_db_chipping_rate*t_length))
     # Calculate code chipping rates with Doppler applied
     # L5I
-    f_l5i_d = L5_chipping_rate*(1. + f_d/L5_freq)
-    f_l5i_dd = L5_chipping_rate*fd_rate/L5_freq
+    f_l5i_d = L5_chipping_rate*(1. + f_d/sig_freq)
+    f_l5i_dd = L5_chipping_rate*fd_rate/sig_freq
     # Neuman sequence
-    f_nh_d = nh_chipping_rate*(1. + f_d/L5_freq)
-    f_nh_dd = nh_chipping_rate*fd_rate/L5_freq
+    f_nh_d = nh_chipping_rate*(1. + f_d/sig_freq)
+    f_nh_dd = nh_chipping_rate*fd_rate/sig_freq
     # Databit
     # Data bit sequence
-    f_db_d = L5_db_chipping_rate*(1. + f_d/L5_freq)
-    f_db_dd = L5_db_chipping_rate*fd_rate/L5_freq
+    f_db_d = L5_db_chipping_rate*(1. + f_d/sig_freq)
+    f_db_dd = L5_db_chipping_rate*fd_rate/sig_freq
     # Calculate the L5I and nh code phase offsets
     l5i_init_code_phase = calcinitcodephase(L5_code_length,
                                             f_l5i_d, f_l5i_dd,
@@ -315,7 +320,7 @@ function definesignal(type::Val{:l5i}, f_s, t_length; prn=1,
                      include_databits, f_l5i_d, f_l5i_dd,
                      f_nh_d, f_nh_dd, f_db_d, f_db_dd, sample_num,
                      isreplica, noexp, L5_chipping_rate,
-                     L5_freq, L5_code_length, db_code_length, databits)
+                     sig_freq, L5_code_length, db_code_length, databits)
 end
 
 
@@ -352,18 +357,19 @@ function definesignal!(signal::L5ISignal;
                        code_start_idx=signal.code_start_idx,
                        isreplica=signal.isreplica,
                        noexp=signal.noexp,
-                       include_databits=signal.include_databits)
+                       include_databits=signal.include_databits,
+                       sig_freq=signal.sig_freq)
     # Calculate code chipping rates with Doppler applied
     # L5I
-    f_l5i_d = L5_chipping_rate*(1. + f_d/L5_freq)
-    f_l5i_dd = L5_chipping_rate*fd_rate/L5_freq
+    f_l5i_d = L5_chipping_rate*(1. + f_d/sig_freq)
+    f_l5i_dd = L5_chipping_rate*fd_rate/sig_freq
     # Neuman sequence
-    f_nh_d = nh_chipping_rate*(1. + f_d/L5_freq)
-    f_nh_dd = nh_chipping_rate*fd_rate/L5_freq
+    f_nh_d = nh_chipping_rate*(1. + f_d/sig_freq)
+    f_nh_dd = nh_chipping_rate*fd_rate/sig_freq
     # Databit
     # Data bit sequence
-    f_db_d = L5_db_chipping_rate*(1. + f_d/L5_freq)
-    f_db_dd = L5_db_chipping_rate*fd_rate/L5_freq
+    f_db_d = L5_db_chipping_rate*(1. + f_d/sig_freq)
+    f_db_dd = L5_db_chipping_rate*fd_rate/sig_freq
     # Calculate the L5I and nh code phase offsets
     l5i_init_code_phase = calcinitcodephase(L5_code_length,
                                             f_l5i_d, f_l5i_dd,
@@ -400,5 +406,6 @@ function definesignal!(signal::L5ISignal;
     signal.isreplica = isreplica
     signal.noexp = noexp
     signal.include_databits = include_databits
+    signal.sig_freq = sig_freq
     return signal
 end
