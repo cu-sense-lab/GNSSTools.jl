6fed536a67634ef51e52eb02940e0fed68c224d6
Sergei Bilardi <sergei.bilardi@colorado.edu>
2020-11-13 13:45:58 -0700

    - Duplicated courseacquisition! function for use with ReplicaSignals struct

--- src/course_acquisition_tools.jl
+++ src/course_acquisition_tools.jl
@@ -37,6 +37,99 @@ Operates in place on `corr_result`.
 Doppler bin.
 """
 function courseacquisition!(corr_result::Array{Float64,2},
+                            data::GNSSSignal, replica::ReplicaSignals,
+                            prn; fd_center=0., fd_range=5000.,
+                            fd_rate=0., Δfd=1/replica.t_length,
+                            message="Correlating...",
+                            operation="replace", start_idx=1,
+                            showprogressbar=true)
+    # Number of data samples
+    dsize = replica.sample_num
+    # Pre-plan FFTs and IFFTs
+    pfft = plan_fft!(replica.data)  # In-place FFT plan
+    pifft = plan_ifft!(replica.data) # In-place IFFT plan
+    # Carrier wipe data signal, make copy, and take FFT
+    datafft = fft(data.data[start_idx:start_idx+dsize-1] .*
+                  exp.(-2π.*data.f_if.*data.t[1:dsize].*1im))
+    # datafft = fft(data.data)
+    # Number of bits representing `data`
+    nADC = data.nADC
+    # Number of Doppler bins
+    doppler_bin_num = Int(fd_range/Δfd*2+1)
+    # Loading bar
+    if showprogressbar
+       p = Progress(doppler_bin_num, 1, message)
+    end
+    @inbounds for i in 1:doppler_bin_num
+        # Calculate Doppler frequency for `i` Doppler bin
+        f_d = (fd_center-fd_range) + (i-1)*Δfd
+        # Set signal parameters
+        definesignal!(replica;
+                      prn=prn, f_d=f_d,
+                      fd_rate=fd_rate, ϕ=0., f_if=0.,
+                      # include_carrier=true,
+                      # include_thermal_noise=false,
+                      # include_phase_noise=false,
+                      # include_adc=false,
+                      code_start_idx=1,
+                      nADC=nADC, isreplica=true,
+                      noexp=false)
+        # Generate signal
+        generatesignal!(replica, replica.isreplica)
+        # Perform in place FFT on replica
+        pfft*replica.data
+        # Take conjugate of FFT(replica) and multiply with FFT of
+        # data. The result is stored in `replica.signal`
+        conjAmultB1D!(replica.data, datafft, dsize)
+        # Take IFFT in place
+        pifft*replica.data
+        # Take `abs2` in place and save result
+        # Either replace, add to, or multiply by pre-existing value
+        # in `corr_result`
+        @threads for j in 1:dsize
+            if operation == "replace"
+                @inbounds corr_result[i,j] = abs2(replica.data[j])
+            elseif operation == "add"
+                @inbounds corr_result[i,j] += abs2(replica.data[j])
+            elseif operation == "multiply"
+                @inbounds corr_result[i,j] *= abs2(replica.data[j])
+            end
+        end
+        # Update progress bar
+        if showprogressbar
+          next!(p)
+        end
+    end
+    # Set `isreplica` flag to false in `replica`
+    definesignal!(replica, isreplica=false)
+    return corr_result
+end
+
+
+"""
+    courseacquisition!(corr_result::Array{Float64,2},
+                       data, replica::ReplicaSignal,
+                       prn; fd_center=0., fd_range=5000.,
+                       fd_rate=0., Δfd=1/data.t_length,
+                       threads=8, message="Correlating...",
+                       operation="replace", start_idx=1)
+
+Performs course acquisition on either `GNSSData` or `ReplicaSignal`
+type struct using defined `ReplicaSignal` type struct. No need
+to use `generatesignal!` before calling this function.
+Operates in place on `corr_result`.
+
+*NOTE:* If `data` is a replica signal structure type,
+        it's structure type must be the same as the type
+        of `replica`. They must also be separately defined
+        or one must be a deep copy of the other. Do not
+        pass the same structure to the `data` and `replica`
+        arguments.
+
+`corr_result` contains |conj(fft(replica)*fft(data)|² per
+Doppler bin.
+"""
+function courseacquisition!(corr_result::Array{Float64,2},
                             data::GNSSSignal, replica::ReplicaSignal,
                             prn; fd_center=0., fd_range=5000.,
                             fd_rate=0., Δfd=1/replica.t_length,
@@ -67,9 +160,9 @@ function courseacquisition!(corr_result::Array{Float64,2},
         definesignal!(replica;
                       prn=prn, f_d=f_d,
                       fd_rate=fd_rate, ϕ=0., f_if=0.,
-                      include_carrier=true,
-                      include_noise=false,
-                      include_adc=false,
+                      # include_carrier=true,
+                      # include_noise=false,
+                      # include_adc=false,
                       code_start_idx=1,
                       nADC=nADC, isreplica=true,
                       noexp=false)
