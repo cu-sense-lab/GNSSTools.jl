ef65fe2ecebf240d67752d8b714f498f6e54422c
Sergei Bilardi <sergei.bilardi@colorado.edu>
2020-11-13 13:47:07 -0700

    - Duplicated fft based fineacquisition function for use with ReplicaSignals struct

--- src/fine_acquisition_tools.jl
+++ src/fine_acquisition_tools.jl
@@ -30,6 +30,113 @@ Performs an FFT based fine acquisition on `data`. Note that `t_length` must
 equal `replica.t_length`. `data` can be either a `GNSSData` or `L5QSignal`
 struct, however, `data` and `replica` must be two seperate structs.
 """
+function fineacquisition(data::GNSSSignal, replica::ReplicaSignals, prn, fd_course,
+                         n₀_idx_course, type::Val{:fft}; fd_rate=0.,
+                         t_length=replica.t_length, freq_lim=10000.,
+                         σω=1000., err_bin_num_ϕ=1, err_bin_num_f=2)
+    # Generate replica
+    # Set signal parameters
+    definesignal!(replica;
+                  prn=prn, f_d=fd_course,
+                  fd_rate=fd_rate, ϕ=0., f_if=0.,
+                  # include_carrier=true,
+                  # include_thermal_noise=false,
+                  # include_phase_noise=false,
+                  # include_adc=false,
+                  code_start_idx=n₀_idx_course,
+                  isreplica=true,
+                  noexp=true)
+    # Generate signal
+    generatesignal!(replica, replica.isreplica)
+    # Wipeoff IF and course Doppler from data and multiply by replica
+    # sig = data.data.*exp.(-2π.*(data.f_if+fd_course).*data.t.*1im).*replica.data
+    @threads for i in 1:replica.sample_num
+        @inbounds replica.data[i] = data.data[i]*exp(-2π*(data.f_if+fd_course)*data.t[i]*1im)*replica.data[i]
+    end
+    # Perform in place FFT of `replica.data`
+    fft!(replica.data)
+    # replica.data = fft(sig)
+    # Find peak within ±[x]kHz, where `x` is defined by freq_lim
+    # From index 1 to N/2: positive frequencies
+    # From index N/2 to N: negative frequencies
+    Δf = 1/replica.t_length
+    N = replica.sample_num
+    N_over_2 = Int64(floor(N/2))
+    N_lim = Int64(floor(freq_lim/Δf))
+    pos_freq_interval = (1, N_lim)
+    neg_freq_interval = (N-N_lim, N)
+    pk_val = 0. + 0im
+    pk_valabs2 = 0.
+    pk_idx = 1
+    #### TODO: Make the peak searching loops below functions and multi-thread them. ####
+    # Check positive frequencies for peak
+    @inbounds for i in pos_freq_interval[1]:pos_freq_interval[2]
+        data_abs2 = abs2(replica.data[i])
+        if data_abs2 > pk_valabs2
+            # global pk_valabs2
+            # global pk_val
+            # global pk_idx
+            pk_valabs2 = data_abs2
+            pk_val = replica.data[i]
+            pk_idx = i
+        end
+    end
+    # Check negative frequencies for peak
+    @inbounds for i in neg_freq_interval[1]:neg_freq_interval[2]
+        data_abs2 = abs2(replica.data[i])
+        if data_abs2 > pk_valabs2
+            # global pk_valabs2
+            # global pk_val
+            # global pk_idx
+            pk_valabs2 = data_abs2
+            pk_val = replica.data[i]
+            pk_idx = i
+        end
+    end
+    # Calculate peak frequency (the Doppler frequency error)
+    if pk_idx > N_over_2
+        fd_fine = (pk_idx-N-1)*Δf
+    else
+        fd_fine = (pk_idx-1)*Δf
+    end
+    fd_est = fd_course + fd_fine
+    # Calculate initial phase
+    ϕ_init = atan(imag(pk_val)/real(pk_val))
+    # Calculate the covariance matrix
+    # We estimate the error to be ±2 Doppler bin for the frequency error
+    # and ±1 for the phase error.
+    pk_low_idx = ((pk_idx-err_bin_num_ϕ)+replica.sample_num)%replica.sample_num
+    pk_high_idx = ((pk_idx+err_bin_num_ϕ)+replica.sample_num)%replica.sample_num
+    if pk_low_idx == 0
+        pk_low_idx = replica.sample_num
+    end
+    if pk_high_idx == 0
+        pk_high_idx = replica.sample_num
+    end
+    pk_low = replica.data[pk_low_idx]
+    pk_high = replica.data[pk_high_idx]
+    ϕ_low = atan(imag(pk_low)/real(pk_low))
+    ϕ_high = atan(imag(pk_high)/real(pk_high))
+    ϕ_init_err = mean([abs(ϕ_init-ϕ_low), abs(ϕ_init-ϕ_high)])
+    replica.isreplica = false
+    P = diagm([ϕ_init_err^2, (2π*err_bin_num_f*Δf)^2, σω^2])
+    R = [ϕ_init_err^2]
+    # Return `FineAcquisitionResults` struct
+    return FineAcquisitionResults(prn, String(:fft), fd_course, fd_rate, n₀_idx_course,
+                                  t_length, fd_fine, fd_est, ϕ_init, "N/A", P, R)
+end
+
+
+"""
+    fineacquisition(data::GNSSSignal, replica::ReplicaSignal, prn, fd_course,
+                    n₀_idx_course, type::Val{:fft}; fd_rate=0.,
+                    t_length=replica.t_length, freq_lim=10000.,
+                    σω=10., err_bin_num_ϕ=1, err_bin_num_f=2)
+
+Performs an FFT based fine acquisition on `data`. Note that `t_length` must
+equal `replica.t_length`. `data` can be either a `GNSSData` or `L5QSignal`
+struct, however, `data` and `replica` must be two seperate structs.
+"""
 function fineacquisition(data::GNSSSignal, replica::ReplicaSignal, prn, fd_course,
                          n₀_idx_course, type::Val{:fft}; fd_rate=0.,
                          t_length=replica.t_length, freq_lim=10000.,
@@ -39,9 +146,9 @@ function fineacquisition(data::GNSSSignal, replica::ReplicaSignal, prn, fd_cours
     definesignal!(replica;
                   prn=prn, f_d=fd_course,
                   fd_rate=fd_rate, ϕ=0., f_if=0.,
-                  include_carrier=true,
-                  include_noise=false,
-                  include_adc=false,
+                  # include_carrier=true,
+                  # include_noise=false,
+                  # include_adc=false,
                   code_start_idx=n₀_idx_course,
                   isreplica=true,
                   noexp=true)
