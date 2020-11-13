10d96dd6fe02fe0cee30036cb0f047885a9c52ec
Sergei Bilardi <sergei.bilardi@colorado.edu>
2020-11-13 13:43:58 -0700

    - Duplicated all generatesignal! functions and made new methods for ReplicaSignals struct 	- Made modifications to allow for use with new replica signal struct 	- Uses pre-prepared thermal and phase noise arrays, which are initialized in definesignal instead of generating new one

--- src/generatesignal.jl
+++ src/generatesignal.jl
@@ -1,4 +1,165 @@
 """
+    generatesignal!(signal::ReplicaSignals,
+                    isreplica::Val{false}=Val(signal.isreplica);
+                    t_length=signal.t_length)
+
+Generates local GNSS signal using paramters defined in a
+`ReplicaSignal` struct.
+
+Generates a signal with carrier, ADC quantization, noise,
+and Neuman sequence.
+
+No need to specify `isreplica`. Set `isreplica` to `true` to
+use alternate method, which ignores all `Bool` flags in `signal`.
+
+Specify `t_length` to be ≲ to `replica.t_length` for different
+signal generation lengths. **NOTE:** The size of the `replica.data`
+array will still be the same size. You will need to keep track
+of the `t_length` you passed to `generatesignal!`.
+"""
+function generatesignal!(signal::ReplicaSignals,
+                         t_length=signal.t_length;
+                         doppler_curve=missing, doppler_t=missing,
+                         message="Generating signal...")
+    # Common parmeters used for entire signal
+    prn = signal.prn
+    Tsys = signal.Tsys
+    CN0 = signal.CN0
+    f_s = signal.f_s
+    f_if = signal.f_if
+    f_d = signal.f_d
+    fd_rate = signal.fd_rate
+    ϕ_init = signal.ϕ
+    if ismissing(doppler_curve)
+        get_ϕ(t) = 2π*(f_if + f_d + 0.5*fd_rate*t)*t + ϕ_init
+        get_code_val(t) = calc_code_val(signal, t)
+    else
+        get_code_val, get_ϕ = get_chips_and_ϕ(signal, doppler_curve;
+                                              doppler_t=doppler_t)
+    end
+    generatesignal!(signal, t_length, get_code_val, get_ϕ)
+end
+
+
+"""
+    generatesignal!(signal::ReplicaSignals, t_length, get_code_val, get_ϕ, thermal_noise)
+
+Generates local GNSS signal using paramters defined in a
+`ReplicaSignal` struct.
+
+Generates a signal with carrier, ADC quantization, noise,
+and Neuman sequence.
+
+No need to specify `isreplica`. Set `isreplica` to `true` to
+use alternate method, which ignores all `Bool` flags in `signal`.
+
+Specify `t_length` to be ≲ to `replica.t_length` for different
+signal generation lengths. **NOTE:** The size of the `replica.data`
+array will still be the same size. You will need to keep track
+of the `t_length` you passed to `generatesignal!`.
+"""
+function generatesignal!(signal::ReplicaSignals, t_length, get_code_val, get_ϕ)
+    # Common parmeters used for entire signal
+    prn = signal.prn
+    Tsys = signal.Tsys
+    CN0 = signal.CN0
+    f_s = signal.f_s
+    B = signal.signal_type.B
+    nADC = signal.nADC
+    include_carrier = signal.include_carrier
+    include_thermal_noise = signal.include_thermal_noise
+    include_phase_noise = signal.include_phase_noise
+    include_adc = signal.include_adc
+    thermal_noise = signal.thermal_noise
+    phase_noise = signal.phase_noise
+    sigtype = eltype(signal.data)
+    adc_scale = 2^(nADC-1)-1
+    carrier_amp = sqrt(2*k*Tsys)*10^(CN0/20)
+    noise_amp = sqrt(k*B*Tsys)
+    N = floor(Int, t_length*f_s)
+    @threads for i in 1:N
+        @inbounds t = signal.t[i]
+        # Generate code value for given signal type
+        code_val = get_code_val(t)
+        # code_val = real(code_val)
+        if include_carrier & include_thermal_noise & include_phase_noise
+            # Calculate code value with carrier, thermal and phase noise
+            ϕ = get_ϕ(t)
+            ϕ_noise = phase_noise[i]
+            @inbounds signal.data[i] = code_val * carrier_amp * cis(ϕ+ϕ_noise) +
+                                       noise_amp * thermal_noise[i]
+        elseif include_carrier & include_thermal_noise & ~include_phase_noise
+            # Calculate code value with carrier and noise
+            ϕ = get_ϕ(t)
+            @inbounds signal.data[i] = code_val * carrier_amp * cis(ϕ) +
+                                       noise_amp * thermal_noise[i]
+        elseif include_carrier & ~include_thermal_noise & ~include_phase_noise
+            # Calculate code value with carrier and no noise
+            ϕ = get_ϕ(t)
+            @inbounds signal.data[i] = code_val * carrier_amp * cis(ϕ)
+        elseif ~include_carrier & include_thermal_noise & ~include_phase_noise
+            # Calculate code value with noise and no carrier
+            @inbounds signal.data[i] = code_val + noise_amp * thermal_noise[i]
+        elseif include_carrier & ~include_thermal_noise & include_phase_noise
+            # Calculate code value with noise and no carrier
+            @inbounds signal.data[i] = code_val * carrier_amp * cis(ϕ+ϕ_noise)
+        else
+            # Calculate code value only
+            @inbounds signal.data[i] = complex(float(code_val))
+        end
+    end
+    # Quantize signal
+    if include_adc
+        sigmax = sqrt(maximum(abs2, signal.data))
+        @threads for i in 1:signal.sample_num
+            @inbounds signal.data[i] = round(signal.data[i]*adc_scale/sigmax)
+        end
+    end
+    return signal
+end
+
+
+"""
+    generatesignal!(signal::ReplicaSignal,
+                    isreplica::Val{true}=Val(signal.isreplica))
+
+Generates local GNSS signal using paramters defined in a
+`ReplicaSignal` struct.
+
+Generates a signal with carrier, ADC quantization, noise,
+and Neuman sequence.
+
+This version is used only when `isreplica` is set to `true`
+in `signal` and ignores all the `include_*` flags in `signal`.
+Exponential without the amplitude is included automatically.
+"""
+function generatesignal!(signal::ReplicaSignals,
+                         # isreplica::Val{true}=Val(signal.isreplica::Bool))
+                         isreplica::Bool)
+    # Common parmeters used for entire signal
+    prn = signal.prn
+    f_d = signal.f_d
+    f_if = signal.f_if
+    fd_rate = signal.fd_rate
+    ϕ = signal.ϕ
+    noexp = signal.noexp
+    @threads for i in 1:signal.sample_num
+        @inbounds t = signal.t[i]
+        # Generate code value for given signal type
+        code_val = calc_code_val(signal, t)
+        # code_val = real(code_val)
+        if noexp
+            @inbounds signal.data[i] = complex(float(code_val))
+        else
+            @inbounds signal.data[i] = (code_val *
+                                        exp((2π*(f_if + f_d + 0.5*fd_rate*t)*t + ϕ)*1im))
+        end
+    end
+    return signal
+end
+
+
+"""
     generatesignal!(signal::ReplicaSignal,
                     isreplica::Val{false}=Val(signal.isreplica);
                     t_length=signal.t_length)
