515658ee8ccd71698ec2f99875cc6d801360d93a
Sergei Bilardi <sergei.bilardi@colorado.edu>
2020-12-06 16:01:09 -0700

    made function for voss phase noise generation method

--- src/generate_phase_noise.jl
+++ src/generate_phase_noise.jl
@@ -1,9 +1,7 @@
 """
-    generate_phase_noise(N; scale=1/10)
+    voss(noise1, noise2, scale, N=length(noise1))
 """
-function generate_phase_noise(N; scale=1/10)
-    noise = randn(N)
-    noise_temp = randn(N)
+function voss(noise1, noise2, scale, N=length(noise1))
     n = 2
     sampled_noise_T = Array{Int}(undef, 0)
     while n <= N
@@ -19,15 +17,25 @@ function generate_phase_noise(N; scale=1/10)
             T = 2*T
         end
         if (idx+T-1) > N
-            view(noise, idx:N) .+= noise_temp[i]
+            view(noise1, idx:N) .+= noise2[i]
         else
-            view(noise, idx:idx+T-1) .+= noise_temp[i]
+            view(noise1, idx:idx+T-1) .+= noise2[i]
         end
         idx += T
     end
     scale_factor = scale/M
-    view(noise, 1:N) .*= scale_factor
-    return noise
+    view(noise1, 1:N) .*= scale_factor
+    return noise1
+end
+
+
+"""
+    generate_phase_noise(N; scale=1/10)
+"""
+function generate_phase_noise(N; scale=1/10)
+    noise = randn(N)
+    noise_temp = randn(N)
+    return voss(noise, noise_temp, scale, N)
 end
 
 
@@ -36,30 +44,7 @@ end
 """
 function generate_phase_noise!(noise, N=length(noise)::Int; scale=1/10)
     noise_temp = randn(N)
-    n = 2
-    sampled_noise_T = Array{Int}(undef, 0)
-    while n <= N
-        push!(sampled_noise_T, n)
-        n = 2*n
-    end
-    M = length(sampled_noise_T)
-    idx = 1
-    T = 2
-    for i in 1:N
-        if idx > N
-            idx = 1
-            T = 2*T
-        end
-        if (idx+T-1) > N
-            view(noise, idx:N) .+= noise_temp[i]
-        else
-            view(noise, idx:idx+T-1) .+= noise_temp[i]
-        end
-        idx += T
-    end
-    scale_factor = scale/M
-    view(noise, 1:N) .*= scale_factor
-    return noise
+    return voss(noise, noise_temp, scale, N)
 end
 
 
@@ -67,29 +52,5 @@ end
     generate_phase_noise!(noise, temp_noise; scale=1/10)
 """
 function generate_phase_noise!(noise, noise_temp::Vector; scale=1/10)
-    n = 2
-    sampled_noise_T = Array{Int}(undef, 0)
-    N = length(noise)
-    while n <= N
-        push!(sampled_noise_T, n)
-        n = 2*n
-    end
-    M = length(sampled_noise_T)
-    idx = 1
-    T = 2
-    for i in 1:N
-        if idx > N
-            idx = 1
-            T = 2*T
-        end
-        if (idx+T-1) > N
-            view(noise, idx:N) .+= noise_temp[i]
-        else
-            view(noise, idx:idx+T-1) .+= noise_temp[i]
-        end
-        idx += T
-    end
-    scale_factor = scale/M
-    view(noise, 1:N) .*= scale_factor
-    return noise
+    return voss(noise, noise_temp, scale, N)
 end
