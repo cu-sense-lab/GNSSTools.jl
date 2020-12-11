034998121b0f23321e232483df814a53089b07bb
Sergei Bilardi <sergei.bilardi@colorado.edu>
2020-12-11 14:16:00 -0700

    Updated docs.

--- src/calcdoppler.jl
+++ src/calcdoppler.jl
@@ -4,20 +4,32 @@
                 julian_date, eop,
                 obs_ecef, sig_freq)
 
-Use `svECItoECEF` to convert satellite state vector
-(r and v) from ECI to ECEF. `orb` is a list of
-orbit objects initialized using `SatelliteToolbox`.
-The first orbit object is the source, `gps_orb`, and
-the second is object the signal is reflected off of,
-`sat_orb`. Only two objects are used. Anything after
-the second index of `orb` is ignored.
+
+Use `svECItoECEF` to convert satellite state vector (r and v) from ECI to ECEF.
+`orb` is a list of orbit objects initialized using `SatelliteToolbox`. The first
+orbit object is the source, `gps_orb`, and the second is object the signal is
+reflected off of, `sat_orb`. Only two objects are used. Anything after the
+second index of `orb` is ignored.
+
+
+Required Arguments:
+
+- `gps_orb::OrbitPropagator`: GPS satellite `OrbitPropagator` struct
+- `sat_orb::OrbitPropagator`: second satellite `OrbitPropagator` struct
+- `julian_date::Float64`: Julian date
+- `eop`:
+- `obs_ecef::Vector`: receiver ECEF position in meters with format `(x, y, z)`
+- `sig_freq::Float64`: signal carrier frequency in Hz
+
+
+Returns:
+
+- `fd_obs::Float64`: receiver observed Doppler frequency in Hz
 """
 function calcdoppler(gps_orb::OrbitPropagator,
                      sat_orb::OrbitPropagator,
                      julian_date, eop,
                      obs_ecef, sig_freq)
-    # gps_orb = init_orbit_propagator(Val{:sgp4}, gps_tle)
-    # sat_orb = init_orbit_propagator(Val{:sgp4}, sat_tle)
     # Propagate orbits to "julian_date"
     gps_orbit, rg, vg = propagate_to_epoch!(gps_orb, julian_date)
     sat_orbit, rs, vs = propagate_to_epoch!(sat_orb, julian_date)
@@ -40,8 +52,45 @@ function calcdoppler(gps_orb::OrbitPropagator,
     # Radial velocity of GPS satellite relative to satellite
     v_s2g = transpose((rg-rs)/norm(rg-rs))*vg
     # Calculate observed Doppler frequency by observer
-    fd_obs = sig_freq*(v_obs + v_g2s + v_s2g)/c  # Hz
-    return -fd_obs
+    fd_obs = -sig_freq*(v_obs + v_g2s + v_s2g)/c  # Hz
+    return fd_obs
+end
+
+
+"""
+    calcdoppler(r::Vector, v::Vector, obs_ecef, sig_freq)
+
+
+Calculates the Doppler frequency for the direct signal case.
+
+
+Required Arguments:
+
+- `rg::Vector`: first object ECEF position vector in meters with format `(x, y, z)`
+- `vg::Vector`: first object ECEF velocity vector in meters with format `(v_x, v_y, v_z)`
+- `rs::Vector`: second object ECEF position vector in meters with format `(x, y, z)`
+- `vs::Vector`: second object ECEF velocity vector in meters with format `(v_x, v_y, v_z)`
+- `julian_date::Float64`: Julian date
+- `obs_ecef::Vector`: receiver ECEF position in meters with format `(x, y, z)`
+- `sig_freq::Float64`: signal carrier frequency in Hz
+
+
+Returns:
+
+- `fd_obs::Float64`: receiver observed Doppler frequency in Hz
+"""
+function calcdoppler(rg::Vector, vg::Vector, rs::Vector, vs::Vector,
+                     obs_ecef, sig_freq)
+    # Calculate observed radial velocities
+    # Radial velocity of GPS satellite relative to observer
+    v_obs = transpose((rs-obs_ecef)/norm(rs-obs_ecef))*vs
+    # Radial velocity of satellite relative to GPS satellite
+    v_g2s = transpose((rs-rg)/norm(rs-rg))*vs
+    # Radial velocity of GPS satellite relative to satellite
+    v_s2g = transpose((rg-rs)/norm(rg-rs))*vg
+    # Calculate observed Doppler frequency by observer
+    fd_obs = -sig_freq*(v_obs + v_g2s + v_s2g)/c  # Hz
+    return fd_obs
 end
 
 
@@ -49,7 +98,22 @@ end
     calcdoppler(orb::OrbitPropagator, julian_date, eop,
                 obs_ecef, sig_freq)
 
+
 Calculates the Doppler frequency for the direct signal case.
+
+
+Required Arguments:
+
+- `orb::OrbitPropagator`: satellite `OrbitPropagator` struct
+- `julian_date::Float64`: Julian date
+- `eop`:
+- `obs_ecef::Vector`: receiver ECEF position in meters with format `(x, y, z)`
+- `sig_freq::Float64`: signal carrier frequency in Hz
+
+
+Returns:
+
+- `fd_obs::Float64`: receiver observed Doppler frequency in Hz
 """
 function calcdoppler(orb::OrbitPropagator, julian_date, eop,
                      obs_ecef, sig_freq)
@@ -65,20 +129,35 @@ function calcdoppler(orb::OrbitPropagator, julian_date, eop,
     # Direct signal case
     v_obs = transpose((r-obs_ecef)/norm(r-obs_ecef))*v
     # Calculate observed Doppler frequency by observer
-    fd_obs = sig_freq*v_obs/c  # Hz
-    return -fd_obs
+    fd_obs = -sig_freq*v_obs/c  # Hz
+    return fd_obs
 end
 
 
 """
-    calcdoppler(r::Vector{Float64}, v::Vector{Float64}, obs_ecef, sig_freq)
+    calcdoppler(r::Vector, v::Vector, obs_ecef, sig_freq)
+
 
 Calculates the Doppler frequency for the direct signal case.
+
+
+Required Arguments:
+
+- `r::Vector`: object ECEF position vector in meters with format `(x, y, z)`
+- `v::Vector`: object ECEF velocity vector in meters with format `(v_x, v_y, v_z)`
+- `julian_date::Float64`: Julian date
+- `obs_ecef::Vector`: receiver ECEF position in meters with format `(x, y, z)`
+- `sig_freq::Float64`: signal carrier frequency in Hz
+
+
+Returns:
+
+- `fd_obs::Float64`: receiver observed Doppler frequency in Hz
 """
-function calcdoppler(r, v, obs_ecef, sig_freq)
+function calcdoppler(r::Vector, v::Vector, obs_ecef, sig_freq)
     # Direct signal case
     v_obs = transpose((r-obs_ecef)/norm(r-obs_ecef))*v
     # Calculate observed Doppler frequency by observer
-    fd_obs = sig_freq*v_obs/c  # Hz
-    return -fd_obs
+    fd_obs = -sig_freq*v_obs/c  # Hz
+    return fd_obs
 end
