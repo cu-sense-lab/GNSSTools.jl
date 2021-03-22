bc923a334cb95dbfc112295e039a43a89aaf3a49
Sergei Bilardi <sergei.bilardi@colorado.edu>
2021-03-22 11:03:08 -0600

    Fixed error in subsample averaging.

--- src/generatesignal.jl
+++ src/generatesignal.jl
@@ -130,20 +130,21 @@ function generatesignal!(signal::ReplicaSignal, t_length, get_code_val, get_ϕ)
         cis_sum = complex(0.)
         if include_carrier
             for j in 0:(upsample_factor-1)
-                code_val, code_ϕ = get_code_val(t+dΔt)
-                ϕ = get_ϕ(t+dΔt)
-                cis_sum += cis(ϕ+code_ϕ)
+                code_val, code_ϕ = get_code_val(t+j*dΔt)
+                ϕ = get_ϕ(t+j*dΔt)
+                # cis_sum += cis(ϕ+code_ϕ)
+                cis_sum += code_val*cis(ϕ)
             end
         else
             for j in 0:(upsample_factor-1)
-                code_val, code_ϕ = get_code_val(t+dΔt)
+                code_val, code_ϕ = get_code_val(t+j*dΔt)
                 cis_sum += cis(code_ϕ)
             end
         end
         cis_sum = cis_sum/upsample_factor
         if include_carrier & include_thermal_noise & include_phase_noise
             # Calculate code value with carrier, thermal and phase noise
-            ϕ = get_ϕ(t)
+            # ϕ = get_ϕ(t)
             ϕ_noise = real(phase_noise[i])
             # @inbounds signal.data[i] = code_val * carrier_amp * cis(ϕ+ϕ_noise) +
             #                            noise_amp * thermal_noise[i]
@@ -151,7 +152,7 @@ function generatesignal!(signal::ReplicaSignal, t_length, get_code_val, get_ϕ)
                                        noise_amp * thermal_noise[i]
         elseif include_carrier & include_thermal_noise & ~include_phase_noise
             # Calculate code value with carrier and noise
-            ϕ = get_ϕ(t)
+            # ϕ = get_ϕ(t)
             # @inbounds signal.data[i] = code_val * carrier_amp * cis(ϕ) +
             #                            noise_amp * thermal_noise[i]
             @inbounds signal.data[i] = carrier_amp * cis_sum +
@@ -163,7 +164,7 @@ function generatesignal!(signal::ReplicaSignal, t_length, get_code_val, get_ϕ)
             @inbounds signal.data[i] = carrier_amp * cis_sum * cis(ϕ_noise)
         elseif include_carrier & ~include_thermal_noise & ~include_phase_noise
             # Calculate code value with carrier and no noise
-            ϕ = get_ϕ(t)
+            # ϕ = get_ϕ(t)
             # @inbounds signal.data[i] = code_val * carrier_amp * cis(ϕ)
             @inbounds signal.data[i] = carrier_amp * cis_sum
         else
