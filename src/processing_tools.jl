2b32f7a69012b1ae7859bb66d64438bfc5ae7cdb
Sergei Bilardi <sergei.bilardi@colorado.edu>
2021-04-02 02:23:09 -0600

    Cleaned up process function some more. Rename a variable to better clarify what it is and added more documentation for calculating the phase measurement covariance matrix, R. Changed the optional parameter flag, fine_acq, to use_fine_acq, for better clarity.

--- src/processing_tools.jl
+++ src/processing_tools.jl
@@ -50,6 +50,7 @@ Optional Arguments:
     * can be either `:fft` of `:carrier`
 - `return_corrresult`: flag, if set to true, will return the course correlation
                        result `(default = false)`
+- `use_fine_acq`: flag to use a fine acquisition method `(default = true)`
 - `acquisiton_T`: integration time used for course acquisition in 
                   seconds `(default = 1e-3)`
 - `fine_acq_T`: integration time used for fine acquisition in
@@ -72,7 +73,7 @@ function process(signal::GNSSSignal, signal_type::SignalType, prn,
                  cov_mult=1, q_a=1, q_mult=1, dynamickf=true, 
                  dll_b=5, state_num=3, fd_rate=0.,  figsize=missing, 
                  saveto=missing, show_plot=true, fine_acq_method=:carrier,
-                 return_corrresult=false, fine_acq=true, σ_phi=π/2,
+                 return_corrresult=false, use_fine_acq=true, σ_phi=π/2,
                  h₀=1e-21, h₋₂=2e-20, acquisiton_T=1e-3, fine_acq_T=10e-3, 
                  tracking_T=1e-3)
     # Set up replica signals. `replica_t_length` is used for
@@ -101,6 +102,13 @@ function process(signal::GNSSSignal, signal_type::SignalType, prn,
                                     fd_range=fd_range,
                                     fd_rate=fd_rate,
                                     return_corrresult=true)
+    # Estimate the C/N₀ and determine the value of the R matrix
+    # since the estimate from the FFT based fine acquisition method
+    # is always too large to use. This will only be used if the FFT
+    # based fine acquisition method is used, or if no fine acquisition
+    # method is used (when `` = false)
+    CN0_est = snr2cn0(SNR_est, replica.t_length)
+    σᵩ² = phase_noise_variance(CN0_est, replica.t_length)
     # Perform fine acquisition using FFT based method
     # Returns structure containing the fine acquisition results,
     # fine Doppler (`fd_est`) and inital phase estimate (`phi_est`).
@@ -108,7 +116,7 @@ function process(signal::GNSSSignal, signal_type::SignalType, prn,
     # for the states and measurements (`P` and `R`, respectively).
     # `σω` is the uncertainty of the Doppler rate. It is set to 1000Hz/s
     # by default.
-    if fine_acq
+    if use_fine_acq
         if fine_acq_method == :fft
             replicalong = definesignal(signal_type, f_s, fine_acq_T;
                                        skip_noise_generation=true,
@@ -124,15 +132,12 @@ function process(signal::GNSSSignal, signal_type::SignalType, prn,
         else
             error("Invalid value for argument `fine_acq_method`.")
         end
-        P = results.P
         if fine_acq_method == :fft
-            # Estimate the C/N₀ and determine the value of the R matrix
-            CN0_est = snr2cn0(SNR_est, replica.t_length)
-            R = [phase_noise_variance(CN0_est, replica.t_length)]
-            P[1] = sqrt(R[1])
-        else
-            R = results.R
+            results.P[1] = σᵩ²
+            results.R[1] = σᵩ²
         end
+        R = results.R
+        P = results.P
         phi_init = results.phi_init
         fd_est = results.fd_est
     else
@@ -150,7 +155,7 @@ function process(signal::GNSSSignal, signal_type::SignalType, prn,
                                skip_noise_generation=true,
                                allocate_noise_vectors=false)
     end
-    trackresults = trackprn(signal, replica, prn, results.phi_init,
+    trackresults = trackprn(signal, replica, prn, phi_init,
                             fd_est, n0_est, P, R; DLL_B=dll_b,
                             state_num=state_num, dynamickf=dynamickf,
                             cov_mult=cov_mult, qₐ=q_a, q_mult=q_mult,
